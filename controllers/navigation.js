085fb25e1ddc1ea8a24d619df5c4bf1730a7d19a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-09-20 18:28:51 -0300

    Testando geração de prompt com as caracteristícas escolhidas pelo user no front

--- controllers/navigation.js
+++ controllers/navigation.js
@@ -1,12 +1,23 @@
 
 //IMPORTING DEPENCÊNCIES
 var express = require('express');
-var router = express.Router()
+var router = express.Router();
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
 
 
 router.get('/homepage', (req, res) =>{
     res.render('./homepage')
 })
 
+router.post('/promptGenerator', (req, res) =>{
+    var formato = req.body.formatosTextos
+
+    console.log(formato)
+    var prompt = `Gere um texto nesse formato: ${formato}`
+    res.render('./prompt', {
+        formatoVar: prompt
+    })
+})
 
 module.exports = router
