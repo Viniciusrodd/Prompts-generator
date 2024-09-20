135a944fc726d473a7063161b65621463f0af587
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-09-20 19:48:16 -0300

    Criando verificações no campos

--- controllers/navigation.js
+++ controllers/navigation.js
@@ -10,14 +10,29 @@ router.get('/homepage', (req, res) =>{
     res.render('./homepage')
 })
 
+
 router.post('/promptGenerator', (req, res) =>{
     var formato = req.body.formatosTextos
 
-    console.log(formato)
-    var prompt = `Gere um texto nesse formato: ${formato}`
-    res.render('./prompt', {
-        formatoVar: prompt
-    })
+    var prompt = `poderia fazer uma reformatação esclarecida desse texto, 
+        com um ${formato}, mantendo ainda seus pontos 
+        importantes, minuncias, e detalhes especificos sobre os pontos,
+        significado de siglas e termos usados, menções sobre algo 
+        ou alguém, datas mencionadas, se houver figuras deixe apenas 
+        uma sinalização de que terá figuras naquela exata parte do texto, 
+        afim que eu apenas compreenda melhor o conteúdo. Entenda que essa 
+        simplificação é para que eu possa ANOTAR em meu caderno então é 
+        importante que eu não perca NENHUMA informação essencial sobre o tema abordado
+        nos titulos e subtitulos do texto:`
+    
+    if(formato == ''){
+        res.render('./homepage')
+    }else{
+        res.render('./prompt', {
+            formatoVar: prompt
+        })    
+    }
 })
+
 
 module.exports = router
