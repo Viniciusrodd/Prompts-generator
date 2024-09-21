5489446ebebc82497bbb18f791a300057b680ee5
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-09-20 22:14:29 -0300

    Fazendo lógica de criação do prompt gerado

--- controllers/navigation.js
+++ controllers/navigation.js
@@ -12,18 +12,26 @@ router.get('/homepage', (req, res) =>{
 
 
 router.post('/promptGenerator', (req, res) =>{
-    var formato = req.body.formatosTextos
+    var formato = req.body.formatosTextos;
+    var objetivo = req.body.objectiveText;
+    var estilo = req.body.estilo;
+    var preferencias = req.body.preferencias;
+    var publicoAlvo = req.body.publicoAlvo;
 
-    var prompt = `poderia fazer uma reformatação esclarecida desse texto, 
-        com um ${formato}, mantendo ainda seus pontos 
-        importantes, minuncias, e detalhes especificos sobre os pontos,
-        significado de siglas e termos usados, menções sobre algo 
-        ou alguém, datas mencionadas, se houver figuras deixe apenas 
-        uma sinalização de que terá figuras naquela exata parte do texto, 
-        afim que eu apenas compreenda melhor o conteúdo. Entenda que essa 
-        simplificação é para que eu possa ANOTAR em meu caderno então é 
-        importante que eu não perca NENHUMA informação essencial sobre o tema abordado
-        nos titulos e subtitulos do texto:`
+
+    var prompt = `poderia fazer um texto com o FORMATO: ${formato}, 
+    mantendo ainda seus pontos importantes, minuncias, 
+    e detalhes especificos sobre os pontos,
+    significado de siglas e termos usados, menções sobre algo 
+    ou alguém, datas mencionadas, se houver figuras deixe apenas 
+    uma sinalização de que terá figuras naquela exata parte do texto, 
+    afim que eu apenas compreenda melhor o conteúdo. 
+    Este é o OBJETIVO desse texto: ${objetivo} o ESTILO de elaboração 
+    desse texto deve ser: ${estilo}, e também 
+    uma PREFERÊNCIA que quero que ele NÃO POSSUA é: ${preferencias},
+    e quero que o PÚBLICO ALVO desse texto seja: ${publicoAlvo}, entenda que é 
+    importante que eu não perca nenhuma informação essencial sobre o tema abordado
+    nos titulos e subtitulos do texto:`
     
     if(formato == ''){
         res.render('./homepage')
