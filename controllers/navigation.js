21076439534061e81e86db303dc5852243228370
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-09-21 18:54:54 -0300

    Reconstruindo folha de estilo da homepage + adicionando novo campo de opções

--- controllers/navigation.js
+++ controllers/navigation.js
@@ -12,6 +12,7 @@ router.get('/homepage', (req, res) =>{
 
 
 router.post('/promptGenerator', (req, res) =>{
+    var assunto = req.body.aboutText;
     var formato = req.body.formatosTextos;
     var objetivo = req.body.objectiveText;
     var estilo = req.body.estilo;
@@ -20,19 +21,20 @@ router.post('/promptGenerator', (req, res) =>{
     var textRef = req.body.textRef;
 
 
-    var prompt = `poderia fazer um texto com o FORMATO: ${formato}, 
-    mantendo ainda seus pontos importantes, minuncias, 
-    e detalhes especificos sobre os pontos,
-    significado de siglas e termos usados, menções sobre algo 
-    ou alguém, datas mencionadas, se houver figuras deixe apenas 
-    uma sinalização de que terá figuras naquela exata parte do texto, 
+    var prompt = `poderia me construir um texto sobre ${assunto} 
+    com o FORMATO: ${formato}, ressaltando seus pontos mais importantes, 
+    minuncias, e detalhes especificos sobre esses pontos, com
+    significado de siglas e termos, menções sobre algo 
+    ou alguém, datas mencionadas, se houver referência de figuras deixe 
+    uma sinalização de sua recomendação na parte do texto em questão, 
     afim que eu apenas compreenda melhor o conteúdo. 
-    Este é o OBJETIVO desse texto: ${objetivo} o ESTILO de elaboração 
-    desse texto deve ser: ${estilo}, e também 
-    uma PREFERÊNCIA que quero que ele NÃO POSSUA é: ${preferencias},
-    e quero que o PÚBLICO ALVO desse texto seja: ${publicoAlvo}, entenda que é 
+    Este é o OBJETIVO desse texto: ${objetivo}, seu ESTILO de elaboração 
+    desse deve ser: ${estilo}, e também uma PREFERÊNCIA pessoal que quero 
+    que ele NÃO POSSUA é: ${preferencias}, além disso desejo
+    que o PÚBLICO ALVO desse texto seja: ${publicoAlvo}, entenda que é 
     importante que eu não perca nenhuma informação essencial sobre o tema abordado
-    nos titulos e subtitulos do texto. texto REF: ${textRef}`
+    nos titulos e subtitulos do texto. Um possível texto como REFERÊNCIA: 
+    ${textRef}`
 
     if(formato == ''){
         res.render('./homepage')
