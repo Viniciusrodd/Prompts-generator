45a64aa5f6e651fa9851afd1545ada469149a2b9
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-19 11:41:27 -0300

    Updating dependencies

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ var ejs = require('ejs');
 var bodyParser = require('body-parser');
 
 
-
 //SETTING EJS AS A VIEW ENGINE
 app.set('view engine', 'ejs');
 
 
-
 //USING A PUBLIC FOLDER AS A STATIC FILES
 app.use(express.static('public'));
-
 
 
 //CONFIG A BODY-PARSER MIDDLEWARE
@@ -22,11 +19,9 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 
-
 //IMPORTING ROUTER ROUTES and CONFIG EXPRESS TO USE ROUTES DEFINED BY ROUTER
 const navigationController = require('./controllers/navigation');
 app.use('/', navigationController)
-
 
 
 //RUNNING SERVER
